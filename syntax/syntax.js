64400bc49944557ff4ff428ca695ec968f05ee60
FabioKido <fabiohenryquemesquita@gmail.com>
2026-01-20 21:07:33 -0300

    syntax introduction

---
+++ syntax/syntax.js
@@ -0,0 +1,80 @@
+// How to Declare variables:
+let x = 5;
+let y = 6;
+
+// How to Compute values:
+let z = x + y;
+
+// I am a Comment. I do Nothing
+
+/**
+ * The JavaScript syntax defines two types of values:
+ * 
+ * Literals (Fixed values): Numbers, Strings
+ * Variables (Variable values)
+ */
+
+10.50
+1001
+
+"John Doe"
+'Mary Doe'
+
+/** 
+ * JavaScript Keywords
+ * 
+ * JavaScript keywords are used to defines actions to be performed.
+ * JavaScript keywords are case-sensitive. 'let' is diferent of 'Let' and 'LET'.
+ */
+
+// The 'let' and 'const' keywords create variables:
+let age = 25;
+const fname = "John"; // Imutável
+
+/**
+ * JavaScript Variables
+ * 
+ * Variables are containers for storing data values.
+ * Variables must be identified with unique names.
+ */
+
+let v;
+v = 5;
+
+/**
+ * JavaScript Identifiers
+ * An identifier is the 'name' you give to a variable.
+  * Rules for identifiers:
+   * 
+   * Must start with a letter, _, or $
+   * Can contain digits after the first character
+   * Cannot be a reserved keyword (let, const, if, etc.)
+   * Are case-sensitive
+ */
+
+/**
+ * JavaScript Operators
+ * 
+ * JavaScript assignment operators (=) assign values to variables:
+ */
+
+let a = 5;
+let b = 6;
+let sum = a + b;
+
+// JavaScript uses arithmetic operators ( + - * / ) to compute values:
+
+5 * 10
+4 / 2
+
+/**
+ * JavaScript Expressions
+ * 
+ * An expression is a combination of values, variables, and operators, which computes to a value.
+ */
+
+(5 + 6) * 10 // 110
+a * 10 // 50
+"John" + " " + "Doe" // 'Jonh Doe' Concatena strings
+
+// JavaScript programmers tend to use lower camel case: 'lastName'
